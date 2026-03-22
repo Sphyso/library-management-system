07942aa541d20e4e635a5544dc52dd31c1c97bf9
Sifiso <sifisohadebe0103@gmail.com>
2026-03-22 17:35:55 +0200

    Implemented book class and part of librarian class

--- src/main/java/org/library/Book.java
+++ src/main/java/org/library/Book.java
@@ -2,4 +2,32 @@ package org.library;
 
 public class Book {
 
+    private String isbn;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String isbn, String title, String author, boolean isAvailable) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public void markAsBorrowed() {
+        this.isAvailable = false;
+    }
+
+    public void markAsReturned() {
+        this.isAvailable = true;
+    }
+
+    public void getDetails() {
+        System.out.println("Book information: " +"\n"+
+                            "Title: " +this.title+ "\n"+
+                            "ISBN: " +this.isbn+ "\n"+
+                            "Author: " +this.author+ "\n"+
+                            "Available: " +this.isAvailable);
+    }
+
 }
--- src/main/java/org/library/Librarian.java
+++ src/main/java/org/library/Librarian.java
@@ -1,5 +1,10 @@
 package org.library;
 
-public class Librarian {
+public class Librarian extends User {
+
+    public Librarian(String userId, String name, String email) {
+        super(userId, name, email);
+    }
+
 
 }
