c3e7c472dd6e87f53ba8a995d9f99218a3698e9e
Sifiso <sihadjhb025@student.wethinkcode.co.za>
2026-03-22 13:21:32 +0200

    made user abstract and added attributes

--- src/main/java/org/library/User.java
+++ src/main/java/org/library/User.java
@@ -1,5 +1,13 @@
 package org.library;
 
-public class User {
+abstract class User {
+    private String userId;
+    private String name;
+    private String email;
 
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
 }
