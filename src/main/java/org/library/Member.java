8dfa93117527ceee07cd9ed2f1e18551068d203b
Sifiso <sifisohadebe0103@gmail.com>
2026-03-22 15:35:45 +0200

    implemented member class functionality

--- src/main/java/org/library/Member.java
+++ src/main/java/org/library/Member.java
@@ -1,5 +1,29 @@
 package org.library;
 
-public class Member {
+import java.util.ArrayList;
 
+public class Member extends User{
+
+    private ArrayList<Book> borrowedBooks;
+    private final int maxLimit = 5;
+
+    public Member(String userId, String name, String email) {
+        super(userId, name, email);
+    }
+
+    public void borrowBook(Book theBook) {
+        if (borrowedBooks.size() < maxLimit) {
+            this.borrowedBooks.add(theBook);
+        } else {
+            System.out.println("Limit for borrowing books has been reached.");
+        }
+    }
+
+    public void returnBook(Book theBook) {
+        if (borrowedBooks.contains(theBook)) {
+            this.borrowedBooks.remove(theBook);
+        } else {
+            System.out.println("User has not borrowed this book.");
+        }
+    }
 }
